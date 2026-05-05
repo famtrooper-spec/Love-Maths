7391a2bc0a70ec99e02f0c8c5d072aeb05d72045
Jacinto Leandro Rodrigues Pereira <famtrooper@gmail.com>
2026-05-05 16:54:27 +0100

    added code for the division section

--- assets/js/script.js
+++ assets/js/script.js
@@ -121,8 +121,9 @@ function displayMultiplyQuestion(operand1, operand2){
     document.getElementById('operator').textContent = "*";
 }  
 function displayDivisionQuestion(operand1, operand2){
-    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById('operand2').textContent = operand2 > operand1 ? operand1 : operand2;
+    operand1 = operand1 * operand2;
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "/";
 }
 
